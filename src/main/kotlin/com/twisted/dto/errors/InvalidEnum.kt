d80589720fe3e5e9b29fc4ff3e8aa62d3a56cc2d
Julio Sansossio <J.Sansossio@payvision.com>
2019-12-23 13:08:45 +0100

    Add custom error to invalid enum

---
+++ src/main/kotlin/com/twisted/dto/errors/InvalidEnum.kt
@@ -0,0 +1,5 @@
+package com.twisted.dto.errors
+
+private const val MESSAGE = "Invalid enum value"
+
+class InvalidEnum(enumValue: String) : RuntimeException("$MESSAGE: $enumValue")
--- src/main/kotlin/com/twisted/enum/GetMapGeyFromValue.kt
+++ src/main/kotlin/com/twisted/enum/GetMapGeyFromValue.kt
@@ -1,9 +1,11 @@
 package com.twisted.enum
 
+import com.twisted.dto.errors.InvalidEnum
+
 fun GetMapGeyFromValue(map: Map<Int, String>, value: String): Int {
     val keys = map.filterValues { it == value }.keys
     if (keys.count() == 0) {
-        throw Exception("Invalid enum value: $value")
+        throw InvalidEnum(value)
     }
     return keys.first()
 }
