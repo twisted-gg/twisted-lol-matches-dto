69656f9bd8668065cbec53cb774c2fe7bee6de7e
Sansossio <juliosansossio@gmail.com>
2020-01-12 19:02:37 +0100

    update name and remove useless info

--- src/main/kotlin/com/twisted/dto/match_listing/matches/summoner/MatchListingSummonerStats.kt
+++ src/main/kotlin/com/twisted/dto/match_listing/matches/summoner/MatchListingSummonerStats.kt
@@ -2,9 +2,6 @@ package com.twisted.dto.match_listing.matches.summoner
 
 data class MatchListingSummonerStats(
     val champLevel: Int,
-    val neutralMinionsKilled: Int,
-    val neutralMinionsKilledEnemyJungle: Int,
-    val neutralMinionsKilledTeamJungle: Int,
-    val totalMinionsKilled: Int,
+    val minionsKilled: Int,
     val wardsPlaced: Int
 )
