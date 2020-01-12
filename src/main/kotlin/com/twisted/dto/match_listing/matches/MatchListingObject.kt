fbf81a571c8479d71f145eb18e6a1127ef046928
Sansossio <juliosansossio@gmail.com>
2020-01-12 15:27:27 +0100

    required data

--- src/main/kotlin/com/twisted/dto/match_listing/matches/MatchListingObject.kt
+++ src/main/kotlin/com/twisted/dto/match_listing/matches/MatchListingObject.kt
@@ -4,9 +4,15 @@ import com.twisted.dto.match_listing.matches.teams.MatchListingTeamObject
 
 data class MatchListingObject(
     val game_id: String,
+    val queue: Int,
+    val mode: Int,
+    val season: Int,
+    val remake: Boolean,
+    val type: Int,
+    val map_id: Int,
     val victory: Boolean,
     val duration: Long,
     val creation: Long,
     val summoner: MatchListingSummonerObject,
     val teams: List<MatchListingTeamObject>
-)
+)a
