c4dd9fd0b2fd70b8fa9571ebc5b314d20185fd3d
Sansossio <juliosansossio@gmail.com>
2020-01-26 23:39:15 +0100

    Update names

--- src/main/kotlin/com/twisted/enum/common/MapsDto.kt
+++ src/main/kotlin/com/twisted/enum/common/MapsDto.kt
@@ -1,18 +1,18 @@
 package com.twisted.enum.common
 
 val MapsDto = mapOf<Int, String>(
-    1 to "Summoner's Rift",
-    2 to "Summoner's Rift",
-    3 to "The Proving Grounds",
-    4 to "Twisted Treeline",
-    8 to "The Crystal Scar",
-    10 to "Twisted Treeline",
-    11 to "Summoner's Rift",
-    12 to "Howling Abyss",
-    14 to "Butcher's Bridge",
-    16 to "Cosmic Ruins",
-    18 to "Valoran City Park",
-    19 to "Substructure 43",
-    20 to "Crash Site",
-    21 to "Nexus Blitz"
+    1 to "SUMMONER_S_RIFT",
+    2 to "SUMMONER_S_RIFT",
+    3 to "THE_PROVING_GROUNDS",
+    4 to "TWISTED_TREELINE",
+    8 to "THE_CRYSTAL_SCAR",
+    10 to "TWISTED_TREELINE",
+    11 to "SUMMONER_S_RIFT",
+    12 to "HOWLING_ABYSS",
+    14 to "BUTCHER_S_BRIDGE",
+    16 to "COSMIC_RUINS",
+    18 to "VALORAN_CITY_PARK",
+    19 to "SUBSTRUCTURE_43",
+    20 to "CRASH_SITE",
+    21 to "NEXUS_BLITZ"
 )
--- src/main/kotlin/com/twisted/enum/common/SeasonDto.kt
+++ src/main/kotlin/com/twisted/enum/common/SeasonDto.kt
@@ -1,18 +1,18 @@
 package com.twisted.enum.common
 
 val SeasonDto = mapOf<Int, String>(
-    0 to "PRESEASON 3",
-    1 to "SEASON 3",
-    2 to "PRESEASON 2014",
-    3 to "SEASON 2014",
-    4 to "PRESEASON 2015",
-    5 to "SEASON 2015",
-    6 to "PRESEASON 2016",
-    7 to "SEASON 2016",
-    8 to "PRESEASON 2017",
-    9 to "SEASON 2017",
-    10 to "PRESEASON 2018",
-    11 to "SEASON 2018",
-    12 to "PRESEASON 2019",
-    13 to "SEASON 2019"
+    0 to "PRESEASON_3",
+    1 to "SEASON_3",
+    2 to "PRESEASON_2014",
+    3 to "SEASON_2014",
+    4 to "PRESEASON_2015",
+    5 to "SEASON_2015",
+    6 to "PRESEASON_2016",
+    7 to "SEASON_2016",
+    8 to "PRESEASON_2017",
+    9 to "SEASON_2017",
+    10 to "PRESEASON_2018",
+    11 to "SEASON_2018",
+    12 to "PRESEASON_2019",
+    13 to "SEASON_2019"
 )
