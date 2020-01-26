54387b648389ee26eb36f8b78721007afd8a6f63
Sansossio <juliosansossio@gmail.com>
2020-01-26 19:09:43 +0100

    Update perks on math listing

--- src/main/kotlin/com/twisted/dto/match_listing/matches/summoner/MatchListingSummonerObject.kt
+++ src/main/kotlin/com/twisted/dto/match_listing/matches/summoner/MatchListingSummonerObject.kt
@@ -1,6 +1,7 @@
 package com.twisted.dto.match_listing.matches.summoner
 
 import com.twisted.dto.match.participant.stats.MatchParticipantKDA
+import com.twisted.dto.match_details.teams.participant.MatchDetailsTeamsParticipantPerks
 
 data class MatchListingSummonerObject(
     val champion: Int,
@@ -8,6 +9,6 @@ data class MatchListingSummonerObject(
     val items: List<Int>,
     val stats: MatchListingSummonerStats,
     val spells: List<Int>,
-    val perks: List<Int>,
+    val perks: MatchDetailsTeamsParticipantPerks,
     val team: Int
 )
