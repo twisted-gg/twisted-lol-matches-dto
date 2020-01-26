1feea004d8a5fc900cb63feab8847c44620ddd71
Sansossio <juliosansossio@gmail.com>
2020-01-26 19:11:55 +0100

    Kills participation

--- src/main/kotlin/com/twisted/dto/match_listing/matches/summoner/MatchListingSummonerStats.kt
+++ src/main/kotlin/com/twisted/dto/match_listing/matches/summoner/MatchListingSummonerStats.kt
@@ -3,5 +3,6 @@ package com.twisted.dto.match_listing.matches.summoner
 data class MatchListingSummonerStats(
     val champLevel: Int,
     val minionsKilled: Int,
-    val wardsPlaced: Int
+    val wardsPlaced: Int,
+    val killsParticipation: Float
 )
