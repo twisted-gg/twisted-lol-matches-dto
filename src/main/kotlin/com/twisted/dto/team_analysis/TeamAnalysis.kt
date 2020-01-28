ec8f4415b69d4a0310efca8cd22a609ae961f684
Sansossio <juliosansossio@gmail.com>
2020-01-28 21:06:20 +0100

    update field name

--- src/main/kotlin/com/twisted/dto/team_analysis/TeamAnalysis.kt
+++ src/main/kotlin/com/twisted/dto/team_analysis/TeamAnalysis.kt
@@ -3,6 +3,6 @@ package com.twisted.dto.team_analysis
 import com.twisted.dto.team_analysis.match_analysis.MatchAnalysis
 
 data class TeamAnalysis(
-    val winTeam: Int,
+    val teamWinner: Int,
     val matchAnalysis: MatchAnalysis
 )
