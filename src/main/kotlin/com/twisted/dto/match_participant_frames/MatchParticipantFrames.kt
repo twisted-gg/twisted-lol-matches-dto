5fede9f6896b95f58608a97582f574abc0524d9a
Sansossio <juliosansossio@gmail.com>
2020-01-31 22:41:51 +0100

    Frames interval

--- src/main/kotlin/com/twisted/dto/match_participant_frames/MatchParticipantFrames.kt
+++ src/main/kotlin/com/twisted/dto/match_participant_frames/MatchParticipantFrames.kt
@@ -2,5 +2,6 @@ package com.twisted.dto.match_participant_frames
 
 data class MatchParticipantFrames(
     val teamId: Int,
+    val framesInterval: Long,
     val participants: List<MatchParticipantFramesParticipant>
 )
